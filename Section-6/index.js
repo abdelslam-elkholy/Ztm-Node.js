49a68a501efca783b5bdc6c51013df4b0bd8c351
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-10-20 16:38:34 +0300

    update

--- Section-6/index.js
+++ Section-6/index.js
@@ -2,6 +2,13 @@ const { parse } = require("csv-parse");
 const fs = require("fs");
 
 const results = [];
+
+const isHabitat = (data) =>
+  data["koi_disposition"] === "CONFIRMED" &&
+  data["koi_insol"] > 0.36 &&
+  data["koi_insol"] < 1.11 &&
+  data["koi_prad"] < 1.6;
+
 fs.createReadStream("kepler_data.csv")
   .pipe(
     parse({
@@ -9,9 +16,9 @@ fs.createReadStream("kepler_data.csv")
       columns: true,
     })
   )
-  .on("data", (data) => results.push(data))
+  .on("data", (data) => isHabitat(data) && results.push(data))
   .on("error", (err) => console.log(err))
   .on("end", () => {
-    console.log(results);
+    console.log(results.length);
     console.log("end");
   });

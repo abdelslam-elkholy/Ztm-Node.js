3bc1693ea30ed19211c349b61095c3d86b5d2946
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-10-22 01:22:39 +0300

    update

--- Section-8/index.js
+++ Section-8/index.js
@@ -6,6 +6,9 @@ const PORT = 3000;
 
 const friendsRoute = require("./Routes/friends.route");
 
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use((req, res, next) => {
   const start = Date.now();
   next();
@@ -13,6 +16,13 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.render("index", {
+    title: "Home Page",
+    items: ["hey", "you"],
+  });
+});
 
 app.use("/friends", friendsRoute);
 app.listen(PORT, () => console.log(`Server Is Working on Port: ${PORT}`));

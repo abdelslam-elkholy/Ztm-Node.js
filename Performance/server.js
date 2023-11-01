6bc1533b41d22a3e86f9cc1b511801363708ced4
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-01 21:42:17 +0200

    update

--- Performance/server.js
+++ Performance/server.js
@@ -1,25 +1,25 @@
 const http = require("http");
-const cluster = require("cluster");
-const os = require("os");
+// const cluster = require("cluster");
+// const os = require("os");
 
 const app = require("./app");
 const PORT = 8080;
 
 const server = http.createServer(app);
 
-if (cluster.isMaster) {
-  const cpus = os.cpus().length;
-  console.log(os.cpus().length);
+// if (cluster.isMaster) {
+//   const cpus = os.cpus().length;
+//   console.log(os.cpus().length);
 
-  for (let i = 0; i < cpus; i++) {
-    cluster.fork();
-  }
-} else {
-  server.listen(PORT, (err, res) => {
-    console.log(`working on port ${PORT}`);
-  });
-}
+//   for (let i = 0; i < cpus; i++) {
+//     cluster.fork();
+//   }
+// } else {
+//   server.listen(PORT, (err, res) => {
+//     console.log(`working on port ${PORT}`);
+//   });
+// }
 
-// server.listen(PORT, (err, res) => {
-//   console.log(`working on port ${PORT}`);
-// });
+server.listen(PORT, (err, res) => {
+  console.log(`working on port ${PORT}`);
+});

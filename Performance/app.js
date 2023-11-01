7400e32c8e8281d5b04ca3744da0e39b091e6b18
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-11-01 20:50:42 +0200

    update

--- Performance/app.js
+++ Performance/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 
+const delay = (timer) => {
+  const start = Date.now();
+
+  while (Date.now() - start < timer) {
+    // delay
+  }
+};
 app.get("/api/", (req, res) => {
   res.send("HEY");
 });
 
 app.get("delay", (req, res) => {
+  delay(10000);
   res.send("HEY DElllllllllaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaay");
 });
 
